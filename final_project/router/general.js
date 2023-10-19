dbc229efc969ab29ef2b5ce67fc9b8be7f69f2b0
Oytun Turk <oytunturk@gmail.com>
2023-10-19 11:26:12 -0700

    isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,10 +22,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
     //return res.status(300).json({message: "Yet to be implemented"});
     const isbn = req.params.isbn;
-    let filtered_books = books.filter((book) => book.isbn === isbn);
-    res.send(JSON.stringify({filtered_books},null,4));
- });
-  
+    if(isbn) {
+        res.send(books[isbn]);
+    } else {
+        return res.status(300).json({message: "Error! ISBN not found"});
+    }
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
