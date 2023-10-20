2216ffa50f66cdce8f8202567a9bdd3df236be3e
Oytun Turk <oytunturk@users.noreply.github.com>
2023-10-19 20:03:30 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,19 +33,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+    const authorName = req.params.author;
+    let filtered_books = Object.values(books).filter((book) => book.author === authorName);
+    res.send(filtered_books);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filtered_books = Object.values(books).filter((book) => book.title === title);
+  res.send(filtered_books);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_books = Object.values(books).filter((book) => book.isbn === isbn);
+    res.send(filtered_books[0].reviews);
 });
 
 module.exports.general = public_users;
