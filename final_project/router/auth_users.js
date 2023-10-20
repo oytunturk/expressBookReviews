1fc3f11fe3e3aa4dcc44c812d44f0b4f721401db
Oytun Turk <oytunturk@gmail.com>
2023-10-20 10:34:14 -0700

    Authentication update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,11 +6,36 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    //write code to check is the username is valid
+    for(var i=0; i<regd_users.length; i++) {
+        if(regd_users[i].username==username) { 
+            return true; 
+        }
+    }
+    return false;
+}
+
+const pwd = (username)=>{ //returns string
+    //returns password of a registered user
+    for(var i=0; i<regd_users.length; i++) {
+        if(regd_users[i].username==username) { 
+            return regd_users[i].password; 
+        }
+    }
+    return "";
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //write code to check if username and password match the one we have in records.
+    if(isValid(username)) {
+        userpassword = getPassword(username);
+        if(userpassword.length>0) {
+            if(userpassword==password) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 //only registered users can login
