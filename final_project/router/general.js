d834ef19d7ab7dbb8a54f4503c3705b37cc209ce
Oytun Turk <oytunturk@gmail.com>
2023-10-21 20:34:46 -0700

    Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,32 +25,70 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+//public_users.get('/',function (req, res) {
   //Write your code here
     //return res.status(300).json({message: "Yet to be implemented"});
-    res.send(JSON.stringify({books},null,4));
+    //res.send(JSON.stringify({books},null,4));
+//});
+
+let myPromise = new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(JSON.stringify({books},null,4))
+    },6000)})
+
+public_users.get('/',function (req, res) {
+    //Write your code here
+      //return res.status(300).json({message: "Yet to be implemented"});
+      //res.send(JSON.stringify({books},null,4));
+      myPromise.then((successMessage) => {
+        res.send(successMessage)
+      })
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-    //return res.status(300).json({message: "Yet to be implemented"});
-    if(req.params.isbn) {
-        const isbn = req.params.isbn;
-        let filtered_books = Object.values(books).filter((book) => book.isbn === isbn);
-        res.send(filtered_books);
-    } else {
-        return res.status(300).json({message: "Error! ISBN not found"});
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
     }
+        else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+
 });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-    const authorName = req.params.author;
-    let filtered_books = Object.values(books).filter((book) => book.author === authorName);
-    res.send(filtered_books);
+    //const authorName = req.params.author;
+    //let filtered_books = Object.values(books).filter((book) => book.author === authorName);
+    //res.send(filtered_books);
+
+    const get_books_author = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    // console.log(author);
+    if (req.params.author.length > 0) {
+        resolve(res.send(books[author]));
+    } else {
+        reject(res.send('Author not found'));
+    }
+    });
+    get_books_author.then(function(){
+        console.log("Promise for Task 12 is resolved");
+    }).catch(function () { 
+        console.log('Author not found');
+    });
 });
 
 // Get all books based on title
